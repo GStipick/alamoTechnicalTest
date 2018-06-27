a22c00b61fa2748074ea9c8fb455016932915a91
George Stipick <g.stipick@att.net>
2018-06-27 14:01:26 -0500

    Added eslint and resolved linting errors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+
+class App extends PureComponent {
+    render() {
+        return (
+            <div className={styles.App}>
+                <header className={styles['App-header']}>
+                    <img src={logo} className={styles['App-logo']} alt="logo" />
+                    <h1 className={styles['App-title']}>Welcome to React</h1>
+                </header>
+                <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+                </p>
+            </div>
+        );
+    }
 }
 
-export default App;
+export { App };
